8f80f72c34a35d84aeb2fcf641a697ba4d477798
mulddang2 <96711699+mulddang2@users.noreply.github.com>
2022-07-27 23:50:15 +0900

    [update] portfolio_01
    1. 설교영상컨텐츠 수정
    2. 탑버튼 크기 수정
    3. 메인 사진 페이지 슬라이드 기능 넣기
    4. 우리 소식에 버튼 추가
    5. 드랍메뉴 기능 수정

--- portfolio_01/script.js
+++ portfolio_01/script.js
@@ -1,4 +1,13 @@
+$(".mainvisual").slick({
+    slidesToShow: 1,
+    infinite: true,
+    //autoplay: true,
+    dots: true,
+    dotsClass: "slick-dots",
+    arrows: false,
+});
 $(document).ready(function () {
+    //갤러리
     $(".gallery_all").slick({
         slidesToShow: 3,
         autoplay: true,
@@ -6,7 +15,6 @@ $(document).ready(function () {
         centerMode: true,
         centerPadding: "100px",
         infinite: true,
-        arrows: true,
         prevArrow: $(".gallery_prev"),
         nextArrow: $(".gallery_next"),
     });
@@ -14,23 +22,36 @@ $(document).ready(function () {
     // 설교영상
     $(".video_all").slick({
         slidesToShow: 3,
-        //autoplay: true,
+        autoplay: true,
         autoplaySpeed: 1500,
         centerMode: true,
         centerPadding: "100px",
         draggable: false,
+        infinite: true,
         // 클래스명 ct2_인 요소를 버튼으로 사용함
         prevArrow: $(".video_prev"),
         nextArrow: $(".video_next"),
     });
 
+    //우리소식
+    $("ul.news").slick({
+        slidesToShow: 3,
+        autoplay: true,
+        loop: true,
+        autoplay: true,
+
+        // 클래스명 ct2_인 요소를 버튼으로 사용함
+        prevArrow: $(".news_prev"),
+        nextArrow: $(".news_next"),
+    });
+});
+
+//탑버튼 기능 추가
+$(document).ready(function () {
     $(".move_top").on("click", function () {
         document.body.scrollTop = 0; // For Safari
         document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
     });
-});
-
-$(document).ready(function () {
     var position = 0;
 
     $(window).scroll(function () {
@@ -48,6 +69,61 @@ $(document).ready(function () {
             },
             300
         );
+    });
+});
+
+$(document).ready(function () {
+    //클릭하면, drop-menu 보이기
+    //드롭이 안보이면 7.59vh(82px) 이다. 보일 때는, 56.94vh(615px)
+    var gnb = document.querySelector(".gnb"),
+        header = document.getElementById("header");
+
+    var headerDrop = $(".header_drop");
+    var dropInner = $(".header_drop_inner");
+    gnb.addEventListener("mouseenter", function () {
+        console.log("mouseenter");
+
+            headerDrop.stop().animate(
+                {
+                    height: vh(56.94),
+                },
+                300
+            );
+            // dropInner.stop().animate(
+            //     {
+            //         height: vh(56.94),
+            //     },
+            //     300
+            // );
+    });
+    header.addEventListener("mouseleave", function () {
+        headerDrop.stop().animate(
+            {
+                height: vh(0),
+            },
+            300
+        );
+        // dropInner.stop().animate(
+        //     {
+        //         height: vh(0),
+        //     },
+        //     300
+        // );
+        // dropInner.classList.remove("show"),
+            // drop.style.height = 0;
+            //list.style.display = 'none'
+            console.log("mouseleave");
+    });
+
+    var menuTitles = document.querySelectorAll(".menu_title");
+
+
+
+    menuTitles.forEach(function(menuTitle) {
+        menuTitle.addEventListener("mouseover", function(evt) {
+            console.log("evt" + evt);
+            //evt.currentTarget.
+        });
     });
 });
 
